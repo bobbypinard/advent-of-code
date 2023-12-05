9eed2df29a93a4e4e4bd0a4b74f5b69b3a4a3c2b
Bobby Pinard <bobby.pinard@gmail.com>
2023-12-04 19:49:37 -0600

    Day 4 complete

--- 2023/04.py
+++ 2023/04.py
@@ -1,25 +1,54 @@
 file = open(r'./04_input.txt')
+total = 0
 
-def get_winning_numbers(line):
-    arr = ''
+def numbers_to_array(line):
+    arr = []
+    num = ''
     for char in line:
-        if char == ':':
-            arr = []
-        if char == '|':
-            return arr
-        arr += char
+        if char.isdigit():
+            num += char
+        elif char.isspace():
+            if num != '':
+                arr.append(int(num))
+                num = ''
+    if num != '':
+        arr.append(int(num))
     return arr
 
-#def get_my_numbers(line):
-
 def truncate(line):
+    arr = ''    
+    first_digit = line.index(':') + 2
+    arr += line[first_digit:]
+    return arr
+
+def check_winners(my, win):
     arr = []
-    first_winner = line.index(':') + 2
-    arr.append(line[first_winner:-1])
+    for num in my:
+        if num in win:
+            arr.append(num)
     return arr
     
 for line in file:
     trunc = truncate(line)
-    winning_numbers = get_winning_numbers(line)
-    #my_numbers = get_my_numbers(line)
-    print(f'{winning_numbers} - ')
+    winning_numbers = numbers_to_array(trunc[0:29])
+    my_numbers = numbers_to_array(trunc[30:])
+    winners = check_winners(my_numbers, winning_numbers)
+
+    count = 1
+    val = 0
+    for winner in winners:
+        if count == 1:
+            val = 1
+        else:
+            val *= 2
+        count += 1
+
+    total += val
+
+    print('trunc - ', trunc)
+    print('My Numbers - ', my_numbers)
+    print('winning_numbers', winning_numbers)
+    print('Winners - ', winners)
+    print()
+
+print(total)
