bbd073bb57b01c6e71a4abda1469e111cb155194
Bobby Pinard <bobby.pinard@gmail.com>
2023-12-10 14:22:56 -0600

    Day 3 completed

--- 2023/03.py
+++ 2023/03.py
@@ -1,128 +1,25 @@
-file = open('./03_input.txt')
-coordinates = [0,0]
-symbols = '*#/@+%=&$-'
-total = 0
+grid = open('03_input.txt').read().splitlines()
+coordinate_set = set()
 
-def set_dict(schematic):
-    y = 0
-    x = 0
-    dictionary = {}
-    for lines in schematic:
-        for char in lines:
-            if char != '\n':
-                dictionary[(x,y)] = [x, y, char]
-                x += 1
-        y += 1
-        x = 0
-    return dictionary
+for r, row in enumerate(grid):
+    for c, column in enumerate(row):
+        if column.isdigit() or column == ".":
+            continue
+        for current_row in range(r - 1, r + 2):
+            for current_column in range(c - 1, c + 2):
+                if current_row < 0 or current_row >= len(grid) or current_column < 0 or current_column >= len(grid[current_row]) or not grid[current_row][current_column].isdigit():
+                    continue
+                while current_column > 0 and grid[current_row][current_column - 1].isdigit():
+                    current_column -= 1
+                coordinate_set.add((current_row, current_column))
 
-def get_x(val):
-    x = val[0]
-    return x
+ns = []
 
-def get_y(val):
-    y = val[1]
-    return y
+for r, c in coordinate_set:
+    s = ""
+    while c < len(grid[r]) and grid[r][c].isdigit():
+        s += grid[r][c]
+        c += 1
+    ns.append(int(s))
 
-def get_char(val):
-    char = val[2]
-    return char
-
-grid = set_dict(file)
-
-def get_n(val):
-    x = get_x(val)
-    y = get_y(val)
-    n = grid[x, (y-1)]
-    return n
-
-def get_number_dir(val):
-    w = get_w(val)
-    e = get_e(val)
-    num_array = [val]
-    if get_char(w).isdigit():
-        num_array.insert(0, w)
-        if get_char(get_w(get_w(val))).isdigit():
-            num_array.insert(0, get_w(w))
-    if get_char(e).isdigit():
-        num_array.append(e)
-        if get_char(get_e(get_e(val))).isdigit():
-            num_array.append(get_e(e))
-    return num_array
-
-def get_ne(val):
-    x = get_x(val)
-    y = get_y(val)
-    ne = grid[(x+1), (y-1)]
-    return ne
-
-def get_e(val):
-    x = get_x(val)
-    y = get_y(val)
-    e = grid[(x+1), y]
-    return e
-
-def get_se(val):
-    x = get_x(val)
-    y = get_y(val)
-    se = grid[(x+1), (y+1)]
-    return se
-
-def get_s(val):
-    x = get_x(val)
-    y = get_y(val)
-    s = grid[x, (y+1)]
-    return s
-
-def get_sw(val):
-    x = get_x(val)
-    y = get_y(val)
-    sw = grid[(x-1), (y+1)]
-    return sw
-
-def get_w(val):
-    x = get_x(val)
-    y = get_y(val)
-    w = grid[(x-1), y]
-    return w
-
-def get_nw(val):
-    x = get_x(val)
-    y = get_y(val)
-    n = grid[(x-1), (y-1)]
-    return n
-
-def set_dir_numbers(val):
-    digits = []
-    
-    print()
-    return digits
-
-def add_nums(val):
-    num = 0
-    for v in val:
-        num += int(get_char(val))
-    return num
-
-tupes = tuple(grid.values())
-nums = []
-for char in tupes:
-    if get_char(char) in symbols:
-        print(get_char(char))
-        if get_char(get_n(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_n(char))))
-        if get_char(get_ne(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_ne(char))))
-        if get_char(get_e(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_e(char))))
-        if get_char(get_se(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_se(char))))
-        if get_char(get_s(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_s(char))))
-        if get_char(get_sw(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_sw(char))))
-        if get_char(get_w(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_w(char))))
-        if get_char(get_nw(char)).isdigit():
-            nums.append(get_char(get_number_dir(get_nw(char))))
-        print(nums)
+print(sum(ns))
