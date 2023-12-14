49ee6d297c60b766d5658d0b8a510b85d12ce2ac
Bobby Pinard <bobby.pinard@gmail.com>
2023-12-14 07:56:28 -0600

    .

---
+++ 2023/14.py
@@ -0,0 +1,6 @@
+grid = open('14_input.txt').read().splitlines()
+grid = list(map("".join, zip(*grid)))
+grid = ["#".join(["".join(sorted(list(group), reverse=True)) for group in row.split("#")]) for row in grid]
+grid = list(map("".join, zip(*grid)))
+
+print(sum(row.count("O") * (len(grid) - r) for r, row in enumerate(grid)))
